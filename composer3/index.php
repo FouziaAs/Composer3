b1b5375d0b025d75f60997feb384f47f16015f4c
ASRI Fouzia <90463028+FouziaAs@users.noreply.github.com>
2021-11-13 18:18:50 +0100

    Add files via upload

---
+++ composer3/index.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+$output = $bessie->say();
+echo $output;
+
+// or just echo the object for direct output
+echo $bessie;
+// or just echo the object for direct output
+
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
